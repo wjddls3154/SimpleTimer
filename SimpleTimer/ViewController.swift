527bc6a66fea8725d4d7ff484fe24484ec99134f
D7702_10 <d7702_10@D7702-10ui-iMac.local>
2018-04-09 14:42:34 +0900

    SimpleTimer

--- SimpleTimer/ViewController.swift
+++ SimpleTimer/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeSwitch: UISwitch!
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     timeSwitch.setOn(false, animated: true)
+        
+        //myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss EEE"
+        timeLabel.text = formatter.string(from: date)
+    }
+    
+    
+    @IBAction func timerStart(_ sender: Any) {
+        if timeSwitch.isOn == true {
+            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        } else {
+            myTimer.invalidate()
+        }
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
